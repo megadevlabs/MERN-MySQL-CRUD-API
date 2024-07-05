c6ef01d15b07e002cbd8863bdabb54f5abfc7490
Md Salahuddin Khan <cisrony@gmail.com>
2024-07-05 22:31:15 +0600

    Product View Update

--- frontend/src/pages/Products.jsx
+++ frontend/src/pages/Products.jsx
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const Products = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios
+      .get('http://localhost:4000/')
+      .then(res => setData(res.data))
+      .catch(error => console.log(error));
+  }, []);
+
   return (
     <div>
       <h2>Welcome to Products Page</h2>
       <Link to="/createProduct" className="create-button">
         + Create Product
       </Link>
-      <div class="table-responsive">
-        <table class="table table-striped table-hover table-borderless table-primary align-middle">
-          <thead class="table-light">
+      <div className="table-responsive">
+        <table className="table table-striped table-hover table-borderless table-primary align-middle">
+          <thead className="table-light">
             <caption className="cap-1">List of Products</caption>
             <tr>
               <th>ID</th>
@@ -20,21 +29,18 @@ const Products = () => {
               <th>Action</th>
             </tr>
           </thead>
-          <tbody class="table-group-divider">
-            <tr class="table-primary">
-              <td scope="row">1</td>
-              <td>Item</td>
-              <td>Item</td>
-              <td>$ 100</td>
-              <td> Read | Edit | Delete</td>
-            </tr>
-            <tr class="table-primary">
-              <td scope="row">2</td>
-              <td>Item</td>
-              <td>Item</td>
-              <td>$ 120</td>
-              <td> Read | Edit | Delete</td>
-            </tr>
+          <tbody className="table-group-divider">
+            {data.map((products, index) => {
+              return (
+                <tr className="table-primary" key={index}>
+                  <td scope="row">{products.id}</td>
+                  <td>{products.productName}</td>
+                  <td>{products.productQty}</td>
+                  <td>$ {products.productPrice}</td>
+                  <td> Read | Edit | Delete</td>
+                </tr>
+              );
+            })}
           </tbody>
           <tfoot></tfoot>
         </table>
