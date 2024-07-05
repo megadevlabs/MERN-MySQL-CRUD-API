5cd6c402a44bc642b8e9d6e2c2875ad8e29a60d0
Md Salahuddin Khan <cisrony@gmail.com>
2024-07-05 22:10:33 +0600

    All CRUD Operations Done

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ const PORT = 4000;
 
 // Routings
 
-// Get Queries
+// Get Data
 app.get('/', (req, res) => {
   const sql = 'SELECT * FROM `products`';
   connection.query(sql, (err, results, fields) => {
@@ -33,6 +33,78 @@ app.get('/', (req, res) => {
       console.log(results); // results contains rows returned by server
       console.log(fields); // fields contains extra meta data about results, if available
     } catch (err) {
+      return res.json({ message: 'Error inside server!' });
+    }
+  });
+});
+
+// Get Specific Student Route
+app.get('/read/:id', (req, res) => {
+  const sql = 'SELECT * FROM products WHERE id=?';
+  const id = req.params.id;
+
+  connection.query(sql, [id], (err, results, fields) => {
+    try {
+      return res.json(results);
+    } catch (error) {
+      return res.json({ message: 'Error inside server!' });
+    }
+  });
+});
+
+// Insert Data
+app.post('/newProduct', (req, res) => {
+  const sql =
+    'INSERT INTO `products`(`productName`,`productQty`,`productPrice`) VALUES (?)';
+  const values = [
+    req.body.productName,
+    req.body.productQty,
+    req.body.productPrice,
+  ];
+  connection.query(sql, [values], (err, results, fields) => {
+    try {
+      return res.json(results);
+      console.log(results); // results contains rows returned by server
+      console.log(fields); // fields contains extra meta data about results, if available
+    } catch (err) {
+      return res.json({ message: 'Error inside server!' });
+    }
+  });
+});
+
+// Update Data
+app.put('/updateProduct/:id', (req, res) => {
+  const sql =
+    'UPDATE `products` SET `productName` = ? ,`productQty` = ? ,`productPrice` = ? WHERE id=?';
+
+  const id = req.params.id;
+  const values = [
+    req.body.productName,
+    req.body.productQty,
+    req.body.productPrice,
+    id,
+  ];
+
+  connection.query(sql, values, (err, results, fields) => {
+    try {
+      return res.json(results);
+      console.log(results); // results contains rows returned by server
+      console.log(fields); // fields contains extra meta data about results, if available
+    } catch (err) {
+      return res.json({ message: 'Error inside server!' });
+    }
+  });
+});
+
+// Delete Student Route
+app.delete('/delete/:id', (req, res) => {
+  const sql = 'DELETE FROM products WHERE id=?';
+  const id = req.params.id;
+
+  connection.query(sql, [id], (err, result) => {
+    try {
+      return res.json(result);
+    } catch (error) {
       return res.json({ message: 'Error inside server!' });
     }
   });
